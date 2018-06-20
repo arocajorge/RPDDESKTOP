d93c4d2a45998cc76362baf3d7aec02f5eba9792
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-06-20 12:39:53 -0500

    .

---
+++ ERP_database/ERP_database/web/Views/VWBAN_001.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW web.VWBAN_001
+AS
+SELECT        ba_Cbte_Ban_tipo.CodTipoCbteBan, ba_Cbte_Ban.IdEmpresa, ba_Cbte_Ban.IdCbteCble, ba_Cbte_Ban.IdTipocbte, ba_Cbte_Ban.IdBanco, ba_Banco_Cuenta.ba_descripcion, ba_Cbte_Ban.cb_Fecha, 
+                         ba_Cbte_Ban.cb_Observacion, ba_Cbte_Ban.Estado, ba_Cbte_Ban.IdTipoNota, ba_tipo_nota.Descripcion AS Descripcion_TipoNota, 'Ninguno' AS NomBeneficiario, ct_cbtecble_det.IdCtaCble, ct_plancta.pc_Cuenta, 
+                         ct_cbtecble_det.dc_Valor, case when ct_cbtecble_det.dc_Valor > 0 then ct_cbtecble_det.dc_Valor else 0 end as dc_Valor_Debe, case when ct_cbtecble_det.dc_Valor < 0 then ABS(ct_cbtecble_det.dc_Valor) else 0 end as dc_Valor_Haber
+FROM            ct_cbtecble_det INNER JOIN
+                         ct_plancta ON ct_cbtecble_det.IdEmpresa = ct_plancta.IdEmpresa AND ct_cbtecble_det.IdCtaCble = ct_plancta.IdCtaCble INNER JOIN
+                         ba_Cbte_Ban_tipo INNER JOIN
+                         ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo ON ba_Cbte_Ban_tipo.CodTipoCbteBan = ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.CodTipoCbteBan INNER JOIN
+                         ba_Cbte_Ban ON ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.IdEmpresa = ba_Cbte_Ban.IdEmpresa AND ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.IdTipoCbteCble = ba_Cbte_Ban.IdTipocbte INNER JOIN
+                         ba_Banco_Cuenta ON ba_Cbte_Ban.IdEmpresa = ba_Banco_Cuenta.IdEmpresa AND ba_Cbte_Ban.IdBanco = ba_Banco_Cuenta.IdBanco INNER JOIN
+                         ba_tipo_nota ON ba_Cbte_Ban.IdEmpresa = ba_tipo_nota.IdEmpresa AND ba_Cbte_Ban.IdTipoNota = ba_tipo_nota.IdTipoNota ON ct_cbtecble_det.IdEmpresa = ba_Cbte_Ban.IdEmpresa AND 
+                         ct_cbtecble_det.IdTipoCbte = ba_Cbte_Ban.IdTipocbte AND ct_cbtecble_det.IdCbteCble = ba_Cbte_Ban.IdCbteCble
+WHERE        (NOT EXISTS
+                             (SELECT        IdEmpresa, Idcancelacion, Secuencia, IdEmpresa_op, IdOrdenPago_op, Secuencia_op, IdEmpresa_op_padre, IdOrdenPago_op_padre, Secuencia_op_padre, IdEmpresa_cxp, IdTipoCbte_cxp, IdCbteCble_cxp, 
+                                                         IdEmpresa_pago, IdTipoCbte_pago, IdCbteCble_pago, MontoAplicado, SaldoAnterior, SaldoActual, Observacion, fechaTransaccion
+                               FROM            cp_orden_pago_cancelaciones AS f
+                               WHERE        (IdEmpresa_pago = ba_Cbte_Ban.IdEmpresa) AND (IdTipoCbte_pago = ba_Cbte_Ban.IdTipocbte) AND (IdCbteCble_pago = ba_Cbte_Ban.IdCbteCble)))
+UNION ALL
+SELECT        ba_Cbte_Ban_tipo.CodTipoCbteBan, ba_Cbte_Ban.IdEmpresa, ba_Cbte_Ban.IdCbteCble, ba_Cbte_Ban.IdTipocbte, ba_Cbte_Ban.IdBanco, ba_Banco_Cuenta.ba_descripcion, ba_Cbte_Ban.cb_Fecha, 
+                         ba_Cbte_Ban.cb_Observacion, ba_Cbte_Ban.Estado, ba_Cbte_Ban.IdTipoNota, ba_tipo_nota.Descripcion AS Descripcion_TipoNota, tb_persona.pe_nombreCompleto, ct_cbtecble_det.IdCtaCble, ct_plancta.pc_Cuenta, 
+                         ct_cbtecble_det.dc_Valor, case when ct_cbtecble_det.dc_Valor > 0 then ct_cbtecble_det.dc_Valor else 0 end as dc_Valor_Debe, case when ct_cbtecble_det.dc_Valor < 0 then ABS(ct_cbtecble_det.dc_Valor) else 0 end as dc_Valor_Haber
+FROM            tb_persona INNER JOIN
+                         cp_orden_pago ON tb_persona.IdPersona = cp_orden_pago.IdPersona INNER JOIN
+                         ba_Cbte_Ban_tipo INNER JOIN
+                         ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo ON ba_Cbte_Ban_tipo.CodTipoCbteBan = ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.CodTipoCbteBan INNER JOIN
+                         ba_Cbte_Ban ON ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.IdEmpresa = ba_Cbte_Ban.IdEmpresa AND ba_Cbte_Ban_tipo_x_ct_CbteCble_tipo.IdTipoCbteCble = ba_Cbte_Ban.IdTipocbte INNER JOIN
+                         ba_Banco_Cuenta ON ba_Cbte_Ban.IdEmpresa = ba_Banco_Cuenta.IdEmpresa AND ba_Cbte_Ban.IdBanco = ba_Banco_Cuenta.IdBanco INNER JOIN
+                         ba_tipo_nota ON ba_Cbte_Ban.IdEmpresa = ba_tipo_nota.IdEmpresa AND ba_Cbte_Ban.IdTipoNota = ba_tipo_nota.IdTipoNota INNER JOIN
+                         cp_orden_pago_cancelaciones ON ba_Cbte_Ban.IdEmpresa = cp_orden_pago_cancelaciones.IdEmpresa_pago AND ba_Cbte_Ban.IdCbteCble = cp_orden_pago_cancelaciones.IdCbteCble_pago AND 
+                         ba_Cbte_Ban.IdTipocbte = cp_orden_pago_cancelaciones.IdTipoCbte_pago ON cp_orden_pago.IdEmpresa = cp_orden_pago_cancelaciones.IdEmpresa_op AND 
+                         cp_orden_pago.IdOrdenPago = cp_orden_pago_cancelaciones.IdOrdenPago_op INNER JOIN
+                         ct_cbtecble_det ON ba_Cbte_Ban.IdEmpresa = ct_cbtecble_det.IdEmpresa AND ba_Cbte_Ban.IdTipocbte = ct_cbtecble_det.IdTipoCbte AND ba_Cbte_Ban.IdCbteCble = ct_cbtecble_det.IdCbteCble INNER JOIN
+                         ct_plancta ON ct_cbtecble_det.IdCtaCble = ct_plancta.IdCtaCble AND ct_cbtecble_det.IdEmpresa = ct_plancta.IdEmpresa
---
+++ ERP_database/ERP_database/web/Views/VWBAN_002_cancelaciones.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW WEB.VWBAN_002_cancelaciones
+AS
+SELECT        ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdEmpresa, ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdTipocbte, ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdCbteCble, 
+                         ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mba_IdEmpresa, ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mba_IdCbteCble, ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mba_IdTipocbte, 
+                         CASE WHEN fa_factura.IdCbteVta IS NULL AND fa_notaCreDeb.IdNota IS NULL THEN 'ING ' + CAST(caj_Caja_Movimiento.IdCbteCble AS VARCHAR(20)) WHEN fa_factura.IdCbteVta IS NULL AND fa_notaCreDeb.IdNota IS NOT NULL
+                          THEN 'NTDB ' + fa_notaCreDeb.CodNota ELSE 'FACT ' + fa_factura.vt_NumFactura END AS Referencia,
+						  CASE WHEN fa_factura.IdCbteVta IS NULL AND fa_notaCreDeb.IdNota IS NULL THEN caj_Caja_Movimiento.cm_observacion WHEN fa_factura.IdCbteVta IS NULL AND fa_notaCreDeb.IdNota IS NOT NULL
+                          THEN fa_notaCreDeb.sc_observacion ELSE fa_factura.vt_Observacion END AS Observacion
+FROM            fa_notaCreDeb RIGHT OUTER JOIN
+                         fa_factura RIGHT OUTER JOIN
+                         cxc_cobro_x_caj_Caja_Movimiento INNER JOIN
+                         cxc_cobro_det ON cxc_cobro_x_caj_Caja_Movimiento.cbr_IdEmpresa = cxc_cobro_det.IdEmpresa AND cxc_cobro_x_caj_Caja_Movimiento.cbr_IdSucursal = cxc_cobro_det.IdSucursal AND 
+                         cxc_cobro_x_caj_Caja_Movimiento.cbr_IdCobro = cxc_cobro_det.IdCobro RIGHT OUTER JOIN
+                         caj_Caja_Movimiento ON cxc_cobro_x_caj_Caja_Movimiento.mcj_IdEmpresa = caj_Caja_Movimiento.IdEmpresa AND cxc_cobro_x_caj_Caja_Movimiento.mcj_IdCbteCble = caj_Caja_Movimiento.IdCbteCble AND 
+                         cxc_cobro_x_caj_Caja_Movimiento.mcj_IdTipocbte = caj_Caja_Movimiento.IdTipocbte RIGHT OUTER JOIN
+                         ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito ON caj_Caja_Movimiento.IdEmpresa = ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdEmpresa AND 
+                         caj_Caja_Movimiento.IdCbteCble = ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdCbteCble AND caj_Caja_Movimiento.IdTipocbte = ba_Caja_Movimiento_x_Cbte_Ban_x_Deposito.mcj_IdTipocbte ON 
+                         fa_factura.IdEmpresa = cxc_cobro_det.IdEmpresa AND fa_factura.IdSucursal = cxc_cobro_det.IdSucursal AND fa_factura.IdBodega = cxc_cobro_det.IdBodega_Cbte AND fa_factura.IdCbteVta = cxc_cobro_det.IdCbte_vta_nota AND
+                          fa_factura.vt_tipoDoc = cxc_cobro_det.dc_TipoDocumento ON fa_notaCreDeb.IdEmpresa = cxc_cobro_det.IdEmpresa AND fa_notaCreDeb.IdSucursal = cxc_cobro_det.IdSucursal AND 
+                         fa_notaCreDeb.IdBodega = cxc_cobro_det.IdBodega_Cbte AND fa_notaCreDeb.IdNota = cxc_cobro_det.IdCbte_vta_nota AND fa_notaCreDeb.CodDocumentoTipo = cxc_cobro_det.dc_TipoDocumento
