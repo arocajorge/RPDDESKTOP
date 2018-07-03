ad5651514aa9cf86de86777155ca54104e94cc8f
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-03 14:05:31 -0500

    .

--- ERP_database/ERP_database/web/Views/VWCXC_001.sql
+++ ERP_database/ERP_database/web/Views/VWCXC_001.sql
@@ -1,16 +1,17 @@
 ﻿CREATE VIEW web.VWCXC_001
 AS
-SELECT cxc_cobro_det.IdEmpresa, cxc_cobro_det.IdSucursal, cxc_cobro_det.IdCobro, cxc_cobro_det.secuencial, cxc_cobro_det.IdBodega_Cbte, cxc_cobro_det.IdCbte_vta_nota, cxc_cobro_det.dc_TipoDocumento, cxc_cobro_det.dc_ValorPago, 
-                  cxc_cobro_tipo.tc_descripcion, tb_persona.IdPersona, tb_persona.pe_nombreCompleto,fa_factura.vt_serie1 +'-'+ fa_factura.vt_serie2 +'-'+  fa_factura.vt_NumFactura vt_NumFactura, fa_factura.vt_fecha, fa_factura.vt_Observacion AS ObservacionFact, cxc_cobro.cr_observacion AS ObservacionCobro, 
-                  cxc_cobro.cr_fecha, fa_cliente_contactos.Nombres AS NombreContacto, fa_cliente_contactos.Direccion, fa_cliente_contactos.Correo, tb_persona.pe_cedulaRuc, cxc_cobro.cr_estado, tb_sucursal.Su_Descripcion, 
-                  tb_banco.ba_descripcion, cr_NumDocumento, cxc_cobro.cr_TotalCobro
-FROM     fa_factura INNER JOIN
-                  cxc_cobro_det INNER JOIN
-                  cxc_cobro ON cxc_cobro_det.IdEmpresa = cxc_cobro.IdEmpresa AND cxc_cobro_det.IdSucursal = cxc_cobro.IdSucursal AND cxc_cobro_det.IdCobro = cxc_cobro.IdCobro INNER JOIN
-                  cxc_cobro_tipo ON cxc_cobro.IdCobro_tipo = cxc_cobro_tipo.IdCobro_tipo INNER JOIN
-                  fa_cliente ON cxc_cobro.IdEmpresa = fa_cliente.IdEmpresa AND cxc_cobro.IdCliente = fa_cliente.IdCliente INNER JOIN
-                  tb_persona ON fa_cliente.IdPersona = tb_persona.IdPersona ON fa_factura.IdEmpresa = cxc_cobro_det.IdEmpresa AND fa_factura.IdSucursal = cxc_cobro_det.IdSucursal AND fa_factura.IdBodega = cxc_cobro_det.IdBodega_Cbte AND 
-                  fa_factura.IdCbteVta = cxc_cobro_det.IdCbte_vta_nota AND fa_factura.vt_tipoDoc = cxc_cobro_det.dc_TipoDocumento INNER JOIN
-                  fa_cliente_contactos ON fa_factura.IdContacto = fa_cliente_contactos.IdContacto AND fa_factura.IdCliente = fa_cliente_contactos.IdCliente AND fa_factura.IdEmpresa = fa_cliente_contactos.IdEmpresa INNER JOIN
-                  tb_sucursal ON cxc_cobro.IdEmpresa = tb_sucursal.IdEmpresa AND cxc_cobro.IdSucursal = tb_sucursal.IdSucursal LEFT OUTER JOIN
-                  tb_banco ON cxc_cobro.IdBanco = tb_banco.IdBanco
+SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_det.secuencial, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, 
+                  dbo.cxc_cobro_det.dc_ValorPago, dbo.cxc_cobro_tipo.tc_descripcion, dbo.tb_persona.IdPersona, dbo.tb_persona.pe_nombreCompleto, 
+                  dbo.fa_factura.vt_serie1 + '-' + dbo.fa_factura.vt_serie2 + '-' + dbo.fa_factura.vt_NumFactura AS vt_NumFactura, dbo.fa_factura.vt_fecha, dbo.fa_factura.vt_Observacion AS ObservacionFact, 
+                  dbo.cxc_cobro.cr_observacion AS ObservacionCobro, dbo.cxc_cobro.cr_fecha, dbo.fa_cliente_contactos.Nombres AS NombreContacto, dbo.fa_cliente_contactos.Direccion, dbo.fa_cliente_contactos.Correo, 
+                  dbo.tb_persona.pe_cedulaRuc, dbo.cxc_cobro.cr_estado, dbo.tb_sucursal.Su_Descripcion, dbo.cxc_cobro.cr_Banco as ba_descripcion, dbo.cxc_cobro.cr_NumDocumento, dbo.cxc_cobro.cr_TotalCobro
+FROM     dbo.fa_factura INNER JOIN
+                  dbo.cxc_cobro_det INNER JOIN
+                  dbo.cxc_cobro ON dbo.cxc_cobro_det.IdEmpresa = dbo.cxc_cobro.IdEmpresa AND dbo.cxc_cobro_det.IdSucursal = dbo.cxc_cobro.IdSucursal AND dbo.cxc_cobro_det.IdCobro = dbo.cxc_cobro.IdCobro INNER JOIN
+                  dbo.cxc_cobro_tipo ON dbo.cxc_cobro.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo INNER JOIN
+                  dbo.fa_cliente ON dbo.cxc_cobro.IdEmpresa = dbo.fa_cliente.IdEmpresa AND dbo.cxc_cobro.IdCliente = dbo.fa_cliente.IdCliente INNER JOIN
+                  dbo.tb_persona ON dbo.fa_cliente.IdPersona = dbo.tb_persona.IdPersona ON dbo.fa_factura.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND dbo.fa_factura.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND 
+                  dbo.fa_factura.IdBodega = dbo.cxc_cobro_det.IdBodega_Cbte AND dbo.fa_factura.IdCbteVta = dbo.cxc_cobro_det.IdCbte_vta_nota AND dbo.fa_factura.vt_tipoDoc = dbo.cxc_cobro_det.dc_TipoDocumento INNER JOIN
+                  dbo.fa_cliente_contactos ON dbo.fa_factura.IdContacto = dbo.fa_cliente_contactos.IdContacto AND dbo.fa_factura.IdCliente = dbo.fa_cliente_contactos.IdCliente AND 
+                  dbo.fa_factura.IdEmpresa = dbo.fa_cliente_contactos.IdEmpresa INNER JOIN
+                  dbo.tb_sucursal ON dbo.cxc_cobro.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.tb_sucursal.IdSucursal
