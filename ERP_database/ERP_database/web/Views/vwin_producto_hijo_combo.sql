3c13b154b2522143341de068374082720bcbf66b
carloscede <carloscede@hotmail.es>
2018-07-12 16:24:29 -0500

    ....

--- ERP_database/ERP_database/web/Views/vwin_producto_hijo_combo.sql
+++ ERP_database/ERP_database/web/Views/vwin_producto_hijo_combo.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW web.vwin_producto_hijo_combo
 AS
-select pro.IdEmpresa, pro.IdProducto, isnull(pro.IdProducto_padre,0)IdProducto_padre, pro.pr_descripcion, pre.nom_presentacion,ca.ca_Categoria, pro.lote_num_lote, pro.lote_fecha_vcto
+select pro.IdEmpresa, pro.IdProducto, isnull(pro.IdProducto_padre,0)IdProducto_padre, pro.pr_descripcion, pre.nom_presentacion,ca.ca_Categoria, pro.lote_num_lote, pro.lote_fecha_vcto,pro.IdUnidadMedida
 from in_Producto as pro
 inner join in_categorias as ca on pro.IdEmpresa = ca.IdEmpresa and pro.IdCategoria = ca.IdCategoria
 inner join in_presentacion as pre on pro.IdEmpresa = pre.IdEmpresa and pro.IdPresentacion = pre.IdPresentacion
