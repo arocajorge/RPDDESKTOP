432ad7e086a838a7c0ce7cc91c05dec2e6d92483
carloscede <carloscede@hotmail.es>
2018-07-21 08:38:55 -0500

    ....

--- ERP/Core.Erp.Data/vwseg_Menu_x_Usuario_x_Empresa.cs
+++ ERP/Core.Erp.Data/vwseg_Menu_x_Usuario_x_Empresa.cs
@@ -12,15 +12,21 @@ namespace Core.Erp.Data
     using System;
     using System.Collections.Generic;
     
-    public partial class seg_Menu_x_Empresa_x_Usuario
+    public partial class vwseg_Menu_x_Usuario_x_Empresa
     {
+        public int IdMenu { get; set; }
+        public Nullable<int> IdMenuPadre { get; set; }
+        public string DescripcionMenu { get; set; }
+        public int PosicionMenu { get; set; }
+        public bool Habilitado { get; set; }
+        public bool Tiene_FormularioAsociado { get; set; }
+        public string nom_Formulario { get; set; }
+        public string nom_Asembly { get; set; }
+        public Nullable<int> nivel { get; set; }
         public int IdEmpresa { get; set; }
         public string IdUsuario { get; set; }
-        public int IdMenu { get; set; }
-        public bool Lectura { get; set; }
-        public bool Escritura { get; set; }
         public bool Eliminacion { get; set; }
-    
-        public virtual seg_usuario seg_usuario { get; set; }
+        public bool Escritura { get; set; }
+        public bool Lectura { get; set; }
     }
 }
