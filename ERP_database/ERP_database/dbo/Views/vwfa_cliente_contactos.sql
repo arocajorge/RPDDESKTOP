fc36ec955ec0a7c4e35dfab4e8a6df63b93d3089
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-02 12:34:14 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Views/vwfa_cliente_contactos.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW vwfa_cliente_contactos
+AS
+SELECT fa_cliente_contactos.IdEmpresa, fa_cliente_contactos.IdCliente, fa_cliente_contactos.IdContacto, fa_cliente_contactos.Nombres, fa_cliente_contactos.Telefono, fa_cliente_contactos.Celular, fa_cliente_contactos.Correo, 
+                  fa_cliente_contactos.Direccion, fa_cliente_contactos.IdCiudad, fa_cliente_contactos.IdParroquia, tb_parroquia.nom_parroquia
+FROM     fa_cliente_contactos LEFT OUTER JOIN
+                  tb_parroquia ON fa_cliente_contactos.IdParroquia = tb_parroquia.IdParroquia
