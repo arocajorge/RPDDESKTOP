def12040f381d143c2f1861c6ea7ce572c505560
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-08-26 14:49:07 -0500

    .

--- ERP_database/ERP_database/web/Views/vwfa_factura_det.sql
+++ ERP_database/ERP_database/web/Views/vwfa_factura_det.sql
@@ -4,9 +4,11 @@ SELECT        dbo.fa_factura_det.IdEmpresa, dbo.fa_factura_det.IdSucursal, dbo.f
                          dbo.fa_factura_det.vt_Precio, dbo.fa_factura_det.vt_PorDescUnitario, dbo.fa_factura_det.vt_DescUnitario, dbo.fa_factura_det.vt_PrecioFinal, dbo.fa_factura_det.vt_Subtotal, dbo.fa_factura_det.vt_iva, dbo.fa_factura_det.vt_total, 
                          dbo.fa_factura_det.vt_estado, dbo.fa_factura_det.vt_detallexItems, dbo.fa_factura_det.vt_por_iva, dbo.fa_factura_det.IdPunto_Cargo, dbo.fa_factura_det.IdPunto_cargo_grupo, dbo.fa_factura_det.IdCod_Impuesto_Iva, 
                          dbo.fa_factura_det.IdCod_Impuesto_Ice, dbo.fa_factura_det.IdCentroCosto, dbo.fa_factura_det.IdCentroCosto_sub_centro_costo, dbo.fa_factura_det.IdEmpresa_pf, dbo.fa_factura_det.IdSucursal_pf, 
-                         dbo.fa_factura_det.IdProforma, dbo.fa_factura_det.Secuencia_pf, dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_num_lote, dbo.in_Producto.lote_fecha_vcto
-FROM            dbo.in_presentacion INNER JOIN
-                         dbo.in_Producto ON dbo.in_presentacion.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_presentacion.IdPresentacion = dbo.in_Producto.IdPresentacion RIGHT OUTER JOIN
+                         dbo.fa_factura_det.IdProforma, dbo.fa_factura_det.Secuencia_pf, dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_num_lote, dbo.in_Producto.lote_fecha_vcto, 
+                         dbo.in_Producto.se_distribuye, dbo.in_ProductoTipo.tp_ManejaInven
+FROM            dbo.in_presentacion RIGHT OUTER JOIN
+                         dbo.in_Producto ON dbo.in_presentacion.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_presentacion.IdPresentacion = dbo.in_Producto.IdPresentacion LEFT OUTER JOIN
+                         dbo.in_ProductoTipo ON dbo.in_Producto.IdEmpresa = dbo.in_ProductoTipo.IdEmpresa AND dbo.in_Producto.IdProductoTipo = dbo.in_ProductoTipo.IdProductoTipo RIGHT OUTER JOIN
                          dbo.fa_factura_det ON dbo.in_Producto.IdEmpresa = dbo.fa_factura_det.IdEmpresa AND dbo.in_Producto.IdProducto = dbo.fa_factura_det.IdProducto
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
@@ -14,7 +16,7 @@ Begin DesignProperties =
    Begin PaneConfigurations = 
       Begin PaneConfiguration = 0
          NumPanes = 4
-         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+         Configuration = "(H (1[67] 4[3] 2[11] 3) )"
       End
       Begin PaneConfiguration = 1
          NumPanes = 3
@@ -80,6 +82,26 @@ Begin DesignProperties =
          Left = 0
       End
       Begin Tables = 
+         Begin Table = "in_presentacion"
+            Begin Extent = 
+               Top = 197
+               Left = 30
+               Bottom = 327
+               Right = 217
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_Producto"
+            Begin Extent = 
+               Top = 0
+               Left = 625
+               Bottom = 552
+               Right = 859
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
          Begin Table = "fa_factura_det"
             Begin Extent = 
                Top = 6
@@ -90,22 +112,12 @@ Begin DesignProperties =
             DisplayFlags = 280
             TopColumn = 0
          End
-         Begin Table = "in_Producto"
+         Begin Table = "in_ProductoTipo"
             Begin Extent = 
-               Top = 34
-               Left = 708
-               Bottom = 164
-               Right = 942
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-         Begin Table = "in_presentacion"
-            Begin Extent = 
-               Top = 270
-               Left = 38
-               Bottom = 400
-               Right = 225
+               Top = 26
+               Left = 979
+               Bottom = 298
+               Right = 1212
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -143,13 +155,21 @@ Begin DesignProperties =
          Filter = 1350
          Or = 1350
          Or = 1350
-         Or = 1350
+       ', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwfa_factura_det';
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 2, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwfa_factura_det';
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane2', @value = N'  Or = 1350
       End
    End
 End
 ', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwfa_factura_det';
-
-
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwfa_factura_det';
 
