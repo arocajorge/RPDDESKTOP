37bd70227c5349a2f263cdd685f6d05541c43f0e
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-08-27 00:45:11 -0500

    ..

--- ERP_database/ERP_database/web/Stored Procedures/SPINV_005.sql
+++ ERP_database/ERP_database/web/Stored Procedures/SPINV_005.sql
@@ -1,4 +1,5 @@
-﻿CREATE PROCEDURE [web].[SPINV_005] 	
+﻿
+CREATE PROCEDURE [web].[SPINV_005] 	
 @IdEmpresa int,
 @IdSucursal_ini int,
 @IdSucursal_fin int,
@@ -182,39 +183,48 @@ SELECT * FROM (
 						 ,[web].[in_SPINV_005].Saldo_fin_cant,([web].[in_SPINV_005].Saldo_fin_cost / iif([web].[in_SPINV_005].Saldo_fin_cant=0,1,[web].[in_SPINV_005].Saldo_fin_cant)) Cost_prom_fin, [web].[in_SPINV_005].Saldo_fin_cost
 						, [web].[in_SPINV_005].IdUsuario, det.dm_observacion, cab.cm_fecha, in_movi_inven_tipo.cm_descripcionCorta as tipo_movi, tb_bodega.cod_bodega, tb_bodega.bo_Descripcion AS nom_bodega, 
 						tb_sucursal.codigo AS cod_sucursal, tb_sucursal.Su_Descripcion AS nom_sucursal, in_Ing_Egr_Inven_det.IdEmpresa_oc, in_Ing_Egr_Inven_det.IdSucursal_oc, 
-						in_Ing_Egr_Inven_det.IdOrdenCompra, cast(cast(cp_orden_giro.co_factura as numeric) as varchar) AS num_factura, tb_persona.pe_nombreCompleto AS nom_proveedor, in_Producto.pr_codigo, 
-						in_Producto.pr_descripcion, in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo, [web].[in_SPINV_005].[IdProductoPadre]
-FROM            tb_persona INNER JOIN
-                         cp_proveedor ON tb_persona.IdPersona = cp_proveedor.IdPersona INNER JOIN
-                         com_ordencompra_local ON cp_proveedor.IdEmpresa = com_ordencompra_local.IdEmpresa AND 
-                         cp_proveedor.IdProveedor = com_ordencompra_local.IdProveedor RIGHT OUTER JOIN
+						in_Ing_Egr_Inven_det.IdOrdenCompra, 
+						
+						IIF(cp_orden_giro.co_factura is null, cast(cast(fa_factura.vt_numFactura as numeric) as varchar(20)), cast(cast(cp_orden_giro.co_factura as numeric) as varchar(20))) AS num_factura, 
+
+						iif(tb_persona.pe_nombreCompleto is null,fa_cliente_contactos.Nombres,tb_persona.pe_nombreCompleto) AS nom_proveedor, in_Producto.pr_codigo, 
+						in_Producto.pr_descripcion + ' '+pre.nom_presentacion + ' ' + in_Producto.lote_num_lote + ' ' + (iif(in_Producto.lote_fecha_vcto is null,'',CONVERT(varchar(10), in_Producto.lote_fecha_vcto, 103))) pr_descripcion, 
+						in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo, [web].[in_SPINV_005].[IdProductoPadre]
+FROM            fa_factura INNER JOIN
+                         fa_factura_x_in_Ing_Egr_Inven ON fa_factura.IdEmpresa = fa_factura_x_in_Ing_Egr_Inven.IdEmpresa_fa AND fa_factura.IdSucursal = fa_factura_x_in_Ing_Egr_Inven.IdSucursal_fa AND 
+                         fa_factura.IdBodega = fa_factura_x_in_Ing_Egr_Inven.IdBodega_fa AND fa_factura.IdCbteVta = fa_factura_x_in_Ing_Egr_Inven.IdCbteVta_fa AND fa_factura.IdEmpresa = fa_factura_x_in_Ing_Egr_Inven.IdEmpresa_fa AND 
+                         fa_factura.IdSucursal = fa_factura_x_in_Ing_Egr_Inven.IdSucursal_fa AND fa_factura.IdBodega = fa_factura_x_in_Ing_Egr_Inven.IdBodega_fa AND fa_factura.IdCbteVta = fa_factura_x_in_Ing_Egr_Inven.IdCbteVta_fa INNER JOIN
+                         fa_cliente_contactos ON fa_factura.IdEmpresa = fa_cliente_contactos.IdEmpresa AND fa_factura.IdCliente = fa_cliente_contactos.IdCliente AND fa_factura.IdContacto = fa_cliente_contactos.IdContacto AND 
+                         fa_factura.IdEmpresa = fa_cliente_contactos.IdEmpresa AND fa_factura.IdCliente = fa_cliente_contactos.IdCliente AND fa_factura.IdContacto = fa_cliente_contactos.IdContacto RIGHT OUTER JOIN
                          in_movi_inve AS cab INNER JOIN
-                         in_movi_inve_detalle AS det ON cab.IdEmpresa = det.IdEmpresa AND cab.IdSucursal = det.IdSucursal AND cab.IdBodega = det.IdBodega AND 
-                         cab.IdMovi_inven_tipo = det.IdMovi_inven_tipo AND cab.IdNumMovi = det.IdNumMovi INNER JOIN
+                         in_movi_inve_detalle AS det ON cab.IdEmpresa = det.IdEmpresa AND cab.IdSucursal = det.IdSucursal AND cab.IdBodega = det.IdBodega AND cab.IdMovi_inven_tipo = det.IdMovi_inven_tipo AND 
+                         cab.IdNumMovi = det.IdNumMovi INNER JOIN
                          in_movi_inven_tipo ON cab.IdEmpresa = in_movi_inven_tipo.IdEmpresa AND cab.IdMovi_inven_tipo = in_movi_inven_tipo.IdMovi_inven_tipo INNER JOIN
                          tb_bodega ON cab.IdEmpresa = tb_bodega.IdEmpresa AND cab.IdSucursal = tb_bodega.IdSucursal AND cab.IdBodega = tb_bodega.IdBodega INNER JOIN
                          tb_sucursal ON tb_bodega.IdEmpresa = tb_sucursal.IdEmpresa AND tb_bodega.IdSucursal = tb_sucursal.IdSucursal INNER JOIN
-                         in_Ing_Egr_Inven_det ON det.IdEmpresa = in_Ing_Egr_Inven_det.IdEmpresa_inv AND det.IdSucursal = in_Ing_Egr_Inven_det.IdSucursal_inv AND 
-                         det.IdBodega = in_Ing_Egr_Inven_det.IdBodega_inv AND det.IdMovi_inven_tipo = in_Ing_Egr_Inven_det.IdMovi_inven_tipo_inv AND 
-                         det.IdNumMovi = in_Ing_Egr_Inven_det.IdNumMovi_inv AND det.Secuencia = in_Ing_Egr_Inven_det.secuencia_inv RIGHT OUTER JOIN
+                         in_Ing_Egr_Inven_det ON det.IdEmpresa = in_Ing_Egr_Inven_det.IdEmpresa_inv AND det.IdSucursal = in_Ing_Egr_Inven_det.IdSucursal_inv AND det.IdBodega = in_Ing_Egr_Inven_det.IdBodega_inv AND 
+                         det.IdMovi_inven_tipo = in_Ing_Egr_Inven_det.IdMovi_inven_tipo_inv AND det.IdNumMovi = in_Ing_Egr_Inven_det.IdNumMovi_inv AND det.Secuencia = in_Ing_Egr_Inven_det.secuencia_inv RIGHT OUTER JOIN
                          in_Producto INNER JOIN
-                         [web].[in_SPINV_005] ON in_Producto.IdEmpresa = [web].[in_SPINV_005].IdEmpresa AND in_Producto.IdProducto = [web].[in_SPINV_005].IdProducto ON 
-                         det.IdEmpresa = [web].[in_SPINV_005].IdEmpresa AND det.IdSucursal = [web].[in_SPINV_005].IdSucursal AND det.IdBodega = [web].[in_SPINV_005].IdBodega AND 
-                         det.IdProducto = [web].[in_SPINV_005].IdProducto INNER JOIN
-                         in_UnidadMedida ON in_UnidadMedida.IdUnidadMedida = in_Producto.IdUnidadMedida_Consumo ON 
-                         com_ordencompra_local.IdEmpresa = in_Ing_Egr_Inven_det.IdEmpresa_oc AND com_ordencompra_local.IdSucursal = in_Ing_Egr_Inven_det.IdSucursal_oc AND 
-                         com_ordencompra_local.IdOrdenCompra = in_Ing_Egr_Inven_det.IdOrdenCompra LEFT OUTER JOIN
+                         web.in_SPINV_005 ON in_Producto.IdEmpresa = web.in_SPINV_005.IdEmpresa AND in_Producto.IdProducto = web.in_SPINV_005.IdProducto ON det.IdEmpresa = web.in_SPINV_005.IdEmpresa AND 
+                         det.IdSucursal = web.in_SPINV_005.IdSucursal AND det.IdBodega = web.in_SPINV_005.IdBodega AND det.IdProducto = web.in_SPINV_005.IdProducto INNER JOIN
+                         in_UnidadMedida ON in_UnidadMedida.IdUnidadMedida = in_Producto.IdUnidadMedida_Consumo ON fa_factura_x_in_Ing_Egr_Inven.IdEmpresa_in_eg_x_inv = in_Ing_Egr_Inven_det.IdEmpresa AND 
+                         fa_factura_x_in_Ing_Egr_Inven.IdSucursal_in_eg_x_inv = in_Ing_Egr_Inven_det.IdSucursal AND fa_factura_x_in_Ing_Egr_Inven.IdMovi_inven_tipo_in_eg_x_inv = in_Ing_Egr_Inven_det.IdMovi_inven_tipo AND 
+                         fa_factura_x_in_Ing_Egr_Inven.IdNumMovi_in_eg_x_inv = in_Ing_Egr_Inven_det.IdNumMovi LEFT OUTER JOIN
+                         tb_persona INNER JOIN
+                         cp_proveedor ON tb_persona.IdPersona = cp_proveedor.IdPersona INNER JOIN
+                         com_ordencompra_local ON cp_proveedor.IdEmpresa = com_ordencompra_local.IdEmpresa AND cp_proveedor.IdProveedor = com_ordencompra_local.IdProveedor ON 
+                         in_Ing_Egr_Inven_det.IdEmpresa_oc = com_ordencompra_local.IdEmpresa AND in_Ing_Egr_Inven_det.IdSucursal_oc = com_ordencompra_local.IdSucursal AND 
+                         in_Ing_Egr_Inven_det.IdOrdenCompra = com_ordencompra_local.IdOrdenCompra LEFT OUTER JOIN
                          cp_Aprobacion_Ing_Bod_x_OC INNER JOIN
                          cp_Aprobacion_Ing_Bod_x_OC_det ON cp_Aprobacion_Ing_Bod_x_OC.IdEmpresa = cp_Aprobacion_Ing_Bod_x_OC_det.IdEmpresa AND 
                          cp_Aprobacion_Ing_Bod_x_OC.IdAprobacion = cp_Aprobacion_Ing_Bod_x_OC_det.IdAprobacion INNER JOIN
-                         cp_orden_giro ON cp_Aprobacion_Ing_Bod_x_OC.IdEmpresa_Ogiro = cp_orden_giro.IdEmpresa AND 
-                         cp_Aprobacion_Ing_Bod_x_OC.IdCbteCble_Ogiro = cp_orden_giro.IdCbteCble_Ogiro AND 
-                         cp_Aprobacion_Ing_Bod_x_OC.IdTipoCbte_Ogiro = cp_orden_giro.IdTipoCbte_Ogiro ON 
-                         in_Ing_Egr_Inven_det.IdEmpresa = cp_Aprobacion_Ing_Bod_x_OC_det.IdEmpresa_Ing_Egr_Inv AND 
-                         in_Ing_Egr_Inven_det.IdSucursal = cp_Aprobacion_Ing_Bod_x_OC_det.IdSucursal_Ing_Egr_Inv AND 
-                         in_Ing_Egr_Inven_det.IdMovi_inven_tipo = cp_Aprobacion_Ing_Bod_x_OC_det.IdMovi_inven_tipo_Ing_Egr_Inv AND 
-                         in_Ing_Egr_Inven_det.IdNumMovi = cp_Aprobacion_Ing_Bod_x_OC_det.IdNumMovi_Ing_Egr_Inv AND 
-                         in_Ing_Egr_Inven_det.Secuencia = cp_Aprobacion_Ing_Bod_x_OC_det.Secuencia_Ing_Egr_Inv
+                         cp_orden_giro ON cp_Aprobacion_Ing_Bod_x_OC.IdEmpresa_Ogiro = cp_orden_giro.IdEmpresa AND cp_Aprobacion_Ing_Bod_x_OC.IdCbteCble_Ogiro = cp_orden_giro.IdCbteCble_Ogiro AND 
+                         cp_Aprobacion_Ing_Bod_x_OC.IdTipoCbte_Ogiro = cp_orden_giro.IdTipoCbte_Ogiro ON in_Ing_Egr_Inven_det.IdEmpresa = cp_Aprobacion_Ing_Bod_x_OC_det.IdEmpresa_Ing_Egr_Inv AND 
+                         in_Ing_Egr_Inven_det.IdSucursal = cp_Aprobacion_Ing_Bod_x_OC_det.IdSucursal_Ing_Egr_Inv AND in_Ing_Egr_Inven_det.IdMovi_inven_tipo = cp_Aprobacion_Ing_Bod_x_OC_det.IdMovi_inven_tipo_Ing_Egr_Inv AND 
+                         in_Ing_Egr_Inven_det.IdNumMovi = cp_Aprobacion_Ing_Bod_x_OC_det.IdNumMovi_Ing_Egr_Inv AND in_Ing_Egr_Inven_det.Secuencia = cp_Aprobacion_Ing_Bod_x_OC_det.Secuencia_Ing_Egr_Inv LEFT OUTER JOIN
+                         in_presentacion AS pre ON in_Producto.IdEmpresa = pre.IdEmpresa AND in_Producto.IdPresentacion = pre.IdPresentacion
+						 
+						 
 						WHERE 
 						det.IdEmpresa=@IdEmpresa
 						and det.IdSucursal=@IdSucursal_ini 
@@ -242,13 +252,18 @@ FROM            tb_persona INNER JOIN
 						,[web].[in_SPINV_005].IdUsuario, '', @Fecha_ini, '' as tipo_movi, tb_bodega.cod_bodega, tb_bodega.bo_Descripcion AS nom_bodega, 
 						tb_sucursal.codigo AS cod_sucursal, tb_sucursal.Su_Descripcion AS nom_sucursal, null, null, 
 						null, null AS num_factura, null AS nom_proveedor, in_Producto.pr_codigo, 
-						in_Producto.pr_descripcion, in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo, [web].[in_SPINV_005].[IdProductoPadre]
+
+						in_Producto.pr_descripcion + ' '+pre.nom_presentacion + ' ' + in_Producto.lote_num_lote + ' ' + (iif(in_Producto.lote_fecha_vcto is null,'',CONVERT(varchar(10), in_Producto.lote_fecha_vcto, 103))) pr_descripcion, 
+
+						in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo, [web].[in_SPINV_005].[IdProductoPadre]
 						FROM            in_Producto INNER JOIN
 						[web].[in_SPINV_005] ON in_Producto.IdEmpresa = [web].[in_SPINV_005].IdEmpresa AND in_Producto.IdProducto = [web].[in_SPINV_005].IdProducto INNER JOIN
 						tb_sucursal INNER JOIN
 						tb_bodega ON tb_sucursal.IdEmpresa = tb_bodega.IdEmpresa AND tb_sucursal.IdSucursal = tb_bodega.IdSucursal ON 
 						[web].[in_SPINV_005].IdEmpresa = tb_bodega.IdEmpresa AND [web].[in_SPINV_005].IdSucursal = tb_bodega.IdSucursal AND [web].[in_SPINV_005].IdBodega = tb_bodega.IdBodega
 						inner join in_UnidadMedida on in_UnidadMedida.IdUnidadMedida = in_Producto.IdUnidadMedida_Consumo
+						
+						LEFT JOIN in_presentacion as pre on in_Producto.IdEmpresa = pre.IdEmpresa and in_Producto.IdPresentacion = pre.IdPresentacion
 						WHERE NOT EXISTS(
 						SELECT cab.IdEmpresa FROM in_movi_inve cab inner join in_movi_inve_detalle det
 						on cab.IdEmpresa = det.IdEmpresa
@@ -280,7 +295,8 @@ FROM            tb_persona INNER JOIN
 									,[web].[in_SPINV_005].IdUsuario, '', @Fecha_ini, '' as tipo_movi, tb_bodega.cod_bodega, tb_bodega.bo_Descripcion AS nom_bodega, 
 									tb_sucursal.codigo AS cod_sucursal, tb_sucursal.Su_Descripcion AS nom_sucursal, null, null, 
 									null, null AS num_factura, null AS nom_proveedor, in_Producto.pr_codigo, 
-									in_Producto.pr_descripcion, in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo,
+									in_Producto.pr_descripcion + ' '+pre.nom_presentacion + ' ' + in_Producto.lote_num_lote + ' ' + (iif(in_Producto.lote_fecha_vcto is null,'',CONVERT(varchar(10), in_Producto.lote_fecha_vcto, 103))) pr_descripcion,
+									in_UnidadMedida.IdUnidadMedida, in_UnidadMedida.Descripcion as nom_unidad_consumo, in_UnidadMedida.cod_alterno as cod_unidad_consumo,
 									[web].[in_SPINV_005].[IdProductoPadre]
 						FROM            [web].[in_SPINV_005] INNER JOIN
 								in_Producto ON [web].[in_SPINV_005].IdEmpresa = in_Producto.IdEmpresa AND [web].[in_SPINV_005].IdProducto = in_Producto.IdProducto INNER JOIN
@@ -288,6 +304,8 @@ FROM            tb_persona INNER JOIN
 								tb_bodega ON tb_sucursal.IdEmpresa = tb_bodega.IdEmpresa AND tb_sucursal.IdSucursal = tb_bodega.IdSucursal ON 
 								[web].[in_SPINV_005].IdEmpresa = tb_bodega.IdEmpresa AND [web].[in_SPINV_005].IdSucursal = tb_bodega.IdSucursal AND [web].[in_SPINV_005].IdBodega = tb_bodega.IdBodega
 								inner join in_UnidadMedida on in_UnidadMedida.IdUnidadMedida = in_Producto.IdUnidadMedida_Consumo
+								LEFT JOIN in_presentacion as pre on in_Producto.IdEmpresa = pre.IdEmpresa and in_Producto.IdPresentacion = pre.IdPresentacion
+
 						WHERE [web].[in_SPINV_005].IdUsuario = @IdUsuario 
 						and @Mostrar_detallado = 0
 						and tb_bodega.IdEmpresa=@IdEmpresa
