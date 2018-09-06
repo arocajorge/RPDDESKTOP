7b78ddbb6b23e444abb4924a0bec575333e5beba
carloscede <carloscede@hotmail.es>
2018-09-06 11:14:05 -0500

    ....

---
+++ ERP_database/ERP_database/dbo/Tables/tbl_TransaccionesAutorizadas.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [web].[tbl_TransaccionesAutorizadas](
+	[IdEmpresa] [int] NOT NULL,
+	[IdTransaccion] [decimal](18, 0) NOT NULL,
+	[IdUsuarioLog] [varchar](50) NOT NULL,
+	[IdUsuarioAut] [varchar](50) NOT NULL,
+	[Observacion] [varchar](1000) NULL,
+	[FechaTransaccion] [date] NOT NULL,
+ CONSTRAINT [PK_tbl_TransaccionesAutorizadas] PRIMARY KEY CLUSTERED 
+(
+	[IdEmpresa] ASC,
+	[IdTransaccion] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
