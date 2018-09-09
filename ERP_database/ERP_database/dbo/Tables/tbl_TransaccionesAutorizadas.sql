9ed2703e29f7e84b70f81c16124265437d73b739
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-09-09 12:47:09 -0500

    .

--- ERP_database/ERP_database/dbo/Tables/tbl_TransaccionesAutorizadas.sql
+++ ERP_database/ERP_database/dbo/Tables/tbl_TransaccionesAutorizadas.sql
@@ -1,13 +1,10 @@
-﻿CREATE TABLE [web].[tbl_TransaccionesAutorizadas](
-	[IdEmpresa] [int] NOT NULL,
-	[IdTransaccion] [decimal](18, 0) NOT NULL,
-	[IdUsuarioLog] [varchar](50) NOT NULL,
-	[IdUsuarioAut] [varchar](50) NOT NULL,
-	[Observacion] [varchar](1000) NULL,
-	[FechaTransaccion] [date] NOT NULL,
- CONSTRAINT [PK_tbl_TransaccionesAutorizadas] PRIMARY KEY CLUSTERED 
-(
-	[IdEmpresa] ASC,
-	[IdTransaccion] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+﻿CREATE TABLE [web].[tbl_TransaccionesAutorizadas] (
+    [IdEmpresa]        INT           NOT NULL,
+    [IdTransaccion]    DECIMAL (18)  NOT NULL,
+    [IdUsuarioLog]     VARCHAR (50)  NOT NULL,
+    [IdUsuarioAut]     VARCHAR (50)  NOT NULL,
+    [Observacion]      VARCHAR (MAX) NULL,
+    [FechaTransaccion] DATETIME      NOT NULL,
+    CONSTRAINT [PK_tbl_TransaccionesAutorizadas] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdTransaccion] ASC)
+);
+
