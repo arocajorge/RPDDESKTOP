2e147709049998a163214149dded4e6e51a15db7
carloscede <carloscede@hotmail.es>
2018-09-18 10:22:37 -0500

    ...

--- ERP_database/ERP_database/Security/Fj_servindustrias.sql
+++ ERP_database/ERP_database/Security/Fj_servindustrias.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Fj_servindustrias]
     AUTHORIZATION [dbo];
 
+
+
--- ERP_database/ERP_database/web/Views/VWFAC_006.sql
+++ ERP_database/ERP_database/web/Views/VWFAC_006.sql
@@ -1,14 +1,12 @@
 ﻿CREATE VIEW [web].[VWFAC_006]
 AS
-SELECT dbo.fa_proforma_det.IdEmpresa, dbo.fa_proforma_det.IdSucursal, dbo.fa_proforma_det.IdProforma, dbo.fa_proforma_det.Secuencia, dbo.fa_TerminoPago.nom_TerminoPago, dbo.fa_proforma.pf_plazo, dbo.fa_proforma.pf_codigo, 
-                  dbo.fa_proforma.pf_fecha, dbo.fa_proforma.estado, dbo.fa_proforma.pf_atencion_a, dbo.fa_Vendedor.Codigo, dbo.fa_Vendedor.Ve_Vendedor, dbo.in_Producto.pr_descripcion, dbo.fa_proforma_det.pd_cantidad, 
-                  dbo.fa_proforma_det.pd_precio, dbo.fa_proforma_det.pd_por_descuento_uni, dbo.fa_proforma_det.pd_descuento_uni, dbo.fa_proforma_det.pd_precio_final, dbo.fa_proforma_det.pd_subtotal, dbo.fa_proforma_det.pd_por_iva, 
-                  dbo.fa_proforma_det.pd_iva, dbo.fa_proforma_det.pd_total, dbo.in_Marca.Descripcion AS nom_marca, dbo.in_presentacion.nom_presentacion AS nom_modelo, dbo.in_Producto.pr_observacion, dbo.fa_proforma_det.IdProducto, 
-                  dbo.fa_proforma.pr_dias_entrega, dbo.fa_proforma.pf_observacion, IIF(dbo.in_Producto.se_distribuye != 1, dbo.in_Producto.lote_num_lote, NULL) lote_num_lote, IIF(dbo.in_Producto.se_distribuye != 1, 
-                  dbo.in_Producto.lote_fecha_vcto, NULL) lote_fecha_vcto, in_Producto.IdProducto_padre
-FROM     dbo.fa_proforma INNER JOIN
-                  dbo.fa_proforma_det ON dbo.fa_proforma.IdEmpresa = dbo.fa_proforma_det.IdEmpresa AND dbo.fa_proforma.IdSucursal = dbo.fa_proforma_det.IdSucursal AND 
-                  dbo.fa_proforma.IdProforma = dbo.fa_proforma_det.IdProforma INNER JOIN
+SELECT     dbo.fa_proforma_det.IdEmpresa, dbo.fa_proforma_det.IdSucursal, dbo.fa_proforma_det.IdProforma, dbo.fa_proforma_det.Secuencia, dbo.fa_TerminoPago.nom_TerminoPago, dbo.fa_proforma.pf_plazo, dbo.fa_proforma.pf_codigo, dbo.fa_proforma.pf_fecha, 
+                  dbo.fa_proforma.estado, dbo.fa_proforma.pf_atencion_a, dbo.fa_Vendedor.Codigo, dbo.fa_Vendedor.Ve_Vendedor, dbo.in_Producto.pr_descripcion + ' ' +dbo.in_presentacion.nom_presentacion as pr_descripcion, dbo.fa_proforma_det.pd_cantidad, dbo.fa_proforma_det.pd_precio, dbo.fa_proforma_det.pd_por_descuento_uni, 
+                  dbo.fa_proforma_det.pd_descuento_uni, dbo.fa_proforma_det.pd_precio_final, dbo.fa_proforma_det.pd_subtotal, dbo.fa_proforma_det.pd_por_iva, dbo.fa_proforma_det.pd_iva, dbo.fa_proforma_det.pd_total, dbo.in_Marca.Descripcion AS nom_marca, 
+                  dbo.in_presentacion.nom_presentacion AS nom_modelo, dbo.in_Producto.pr_observacion, dbo.fa_proforma_det.IdProducto, dbo.fa_proforma.pr_dias_entrega, dbo.fa_proforma.pf_observacion, IIF(dbo.in_Producto.se_distribuye != 1, 
+                  dbo.in_Producto.lote_num_lote, NULL) lote_num_lote, IIF(dbo.in_Producto.se_distribuye != 1, dbo.in_Producto.lote_fecha_vcto, NULL) lote_fecha_vcto, in_Producto.IdProducto_padre
+FROM        dbo.fa_proforma INNER JOIN
+                  dbo.fa_proforma_det ON dbo.fa_proforma.IdEmpresa = dbo.fa_proforma_det.IdEmpresa AND dbo.fa_proforma.IdSucursal = dbo.fa_proforma_det.IdSucursal AND dbo.fa_proforma.IdProforma = dbo.fa_proforma_det.IdProforma INNER JOIN
                   dbo.in_Producto ON dbo.fa_proforma_det.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.fa_proforma_det.IdProducto = dbo.in_Producto.IdProducto INNER JOIN
                   dbo.fa_Vendedor ON dbo.fa_proforma.IdEmpresa = dbo.fa_Vendedor.IdEmpresa AND dbo.fa_proforma.IdVendedor = dbo.fa_Vendedor.IdVendedor INNER JOIN
                   dbo.fa_TerminoPago ON dbo.fa_proforma.IdTerminoPago = dbo.fa_TerminoPago.IdTerminoPago INNER JOIN
