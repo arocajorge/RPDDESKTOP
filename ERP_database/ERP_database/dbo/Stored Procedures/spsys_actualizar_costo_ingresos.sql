c3fc7cbb06c515db8f0d01d540a7de510b448736
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-10-31 14:58:52 -0500

    .

--- ERP_database/ERP_database/dbo/Stored Procedures/spsys_actualizar_costo_ingresos.sql
+++ ERP_database/ERP_database/dbo/Stored Procedures/spsys_actualizar_costo_ingresos.sql
@@ -1,12 +1,13 @@
-﻿CREATE PROCEDURE spsys_actualizar_costo_ingresos
+﻿
+CREATE PROCEDURE [dbo].[spsys_actualizar_costo_ingresos]
 AS
-update in_movi_inve_detalle set mv_costo = 0, mv_costo_sinConversion = 0
+update in_movi_inve_detalle set mv_costo = a.Costo, mv_costo_sinConversion = a.Costo
 from (
 select * from tbsys_ProductosConCosto0 as f
 ) a where in_movi_inve_detalle.IdEmpresa = a.IdEmpresa
 and in_movi_inve_detalle.IdProducto = a.IdProducto
 
-update in_Ing_Egr_Inven_det set mv_costo = 0, mv_costo_sinConversion = 0
+update in_Ing_Egr_Inven_det set mv_costo = a.Costo, mv_costo_sinConversion = a.Costo
 from (
 select * from tbsys_ProductosConCosto0 as f
 ) a where in_Ing_Egr_Inven_det.IdEmpresa = a.IdEmpresa
--- ERP_database/ERP_database/dbo/Views/vwro_permiso_x_empleado_1.sql
+++ ERP_database/ERP_database/dbo/Views/vwro_permiso_x_empleado_1.sql
@@ -1,12 +1,15 @@
-﻿CREATE VIEW dbo.XXXINGRESOSCOSTO0
+﻿CREATE VIEW dbo.vwro_permiso_x_empleado
 AS
-SELECT dbo.in_Producto.IdEmpresa, dbo.in_movi_inve_detalle.IdProducto, dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_num_lote, dbo.in_Producto.lote_fecha_vcto
-FROM     dbo.in_presentacion INNER JOIN
-                  dbo.in_Producto ON dbo.in_presentacion.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_presentacion.IdPresentacion = dbo.in_Producto.IdPresentacion RIGHT OUTER JOIN
-                  dbo.in_movi_inve_detalle ON dbo.in_Producto.IdEmpresa = dbo.in_movi_inve_detalle.IdEmpresa AND dbo.in_Producto.IdProducto = dbo.in_movi_inve_detalle.IdProducto
-WHERE  (dbo.in_movi_inve_detalle.mv_tipo_movi = N'+') AND (dbo.in_movi_inve_detalle.mv_costo = 0) AND (dbo.in_Producto.se_distribuye = 0)
+SELECT        dbo.ro_permiso_x_empleado.IdEmpresa, dbo.ro_permiso_x_empleado.IdPermiso, dbo.ro_permiso_x_empleado.IdEmpleado, dbo.ro_permiso_x_empleado.IdEmpleadoAprueba, dbo.ro_permiso_x_empleado.FechaInicio, 
+                         dbo.ro_permiso_x_empleado.FechaFin, dbo.ro_permiso_x_empleado.HoraSalida, dbo.ro_permiso_x_empleado.HoraRegreso, dbo.ro_permiso_x_empleado.DescuentaVacaciones, dbo.ro_permiso_x_empleado.Recuperable, 
+                         dbo.ro_permiso_x_empleado.Asunto, dbo.ro_permiso_x_empleado.Descripcion, dbo.ro_permiso_x_empleado.TipoPermiso, dbo.ro_permiso_x_empleado.estado, dbo.tb_persona.pe_cedulaRuc, 
+                         dbo.tb_persona.pe_nombreCompleto
+FROM            dbo.ro_empleado INNER JOIN
+                         dbo.ro_permiso_x_empleado ON dbo.ro_empleado.IdEmpresa = dbo.ro_permiso_x_empleado.IdEmpresa AND dbo.ro_empleado.IdEmpresa = dbo.ro_permiso_x_empleado.IdEmpresa AND 
+                         dbo.ro_empleado.IdEmpleado = dbo.ro_permiso_x_empleado.IdEmpleadoAprueba AND dbo.ro_empleado.IdEmpleado = dbo.ro_permiso_x_empleado.IdEmpleado INNER JOIN
+                         dbo.tb_persona ON dbo.ro_empleado.IdPersona = dbo.tb_persona.IdPersona
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'XXXINGRESOSCOSTO0';
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwro_permiso_x_empleado';
 
 
 GO
@@ -15,7 +18,7 @@ Begin DesignProperties =
    Begin PaneConfigurations = 
       Begin PaneConfiguration = 0
          NumPanes = 4
-         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+         Configuration = "(H (1[67] 4[5] 2[11] 3) )"
       End
       Begin PaneConfiguration = 1
          NumPanes = 3
@@ -81,32 +84,32 @@ Begin DesignProperties =
          Left = 0
       End
       Begin Tables = 
-         Begin Table = "in_presentacion"
+         Begin Table = "ro_empleado"
             Begin Extent = 
-               Top = 7
-               Left = 48
-               Bottom = 170
-               Right = 264
+               Top = 6
+               Left = 38
+               Bottom = 180
+               Right = 327
             End
             DisplayFlags = 280
             TopColumn = 0
          End
-         Begin Table = "in_Producto"
+         Begin Table = "ro_permiso_x_empleado"
             Begin Extent = 
-               Top = 175
-               Left = 48
-               Bottom = 338
-               Right = 323
+               Top = 6
+               Left = 365
+               Bottom = 302
+               Right = 568
             End
             DisplayFlags = 280
-            TopColumn = 0
+            TopColumn = 1
          End
-         Begin Table = "in_movi_inve_detalle"
+         Begin Table = "tb_persona"
             Begin Extent = 
-               Top = 343
-               Left = 48
-               Bottom = 506
-               Right = 357
+               Top = 20
+               Left = 705
+               Bottom = 294
+               Right = 937
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -118,37 +121,37 @@ Begin DesignProperties =
    Begin DataPane = 
       Begin ParameterDefaults = ""
       End
-      Begin ColumnWidths = 9
+      Begin ColumnWidths = 11
          Width = 284
-         Width = 1200
-         Width = 1200
-         Width = 1200
-         Width = 1200
-         Width = 1200
-         Width = 1200
-         Width = 1200
-         Width = 1200
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
       End
    End
    Begin CriteriaPane = 
       Begin ColumnWidths = 11
          Column = 1440
          Alias = 900
-         Table = 1176
+         Table = 1170
          Output = 720
          Append = 1400
          NewValue = 1170
-         SortType = 1356
-         SortOrder = 1416
+         SortType = 1350
+         SortOrder = 1410
          GroupBy = 1350
-         Filter = 1356
+         Filter = 1350
          Or = 1350
          Or = 1350
          Or = 1350
       End
    End
 End
-', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'XXXINGRESOSCOSTO0';
-
-
+', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwro_permiso_x_empleado';
 
